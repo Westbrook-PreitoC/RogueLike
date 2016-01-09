cd9f63e8111228628fcd7a8c19177818e98300e8
Cameron Preito <cameron.preito@gmail.com>
2016-01-08 17:13:35 -0800

    commit

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardController : MonoBehaviour {
+	
+	public int columns;
+	public int rows;
+	
+	public GameObject floor;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
