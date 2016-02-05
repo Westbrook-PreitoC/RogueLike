121834f2f6b226b1e0d24821ef7ff2d9358ba4fd
Cameron Preito <cameron.preito@gmail.com>
2016-02-05 11:38:01 -0800

    commit

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	public static SoundController Instance;
+
+	void Awake () {
+		if (Instance != null && Instance != this) 
+		{
+			DestroyImmediate(gameObject);
+			return;
+		}
+
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+	
+}
